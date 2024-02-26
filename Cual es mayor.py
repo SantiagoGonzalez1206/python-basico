59dd8592e78421bb0017f6be4985d950acf0427d
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-02-26 09:24:08 -0500

    tuplas.py

---
+++ Cual es mayor.py
@@ -0,0 +1,4 @@
+num1 = input("ingrese el numero 1: ")
+num2 = input("ingrese el numero 2: ")
+num3 = input("ingrese el numero 3: ")
+
---
+++ Suma de dos numeros.py
@@ -0,0 +1,6 @@
+num1 = input("ingrese el numero 1: ")
+num2 = input("ingrese el numero 2: ")
+suma = int(num1) + int(num2)
+print(suma)
+
+
